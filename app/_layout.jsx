86b5bd26fe401743d606d36f51d178b463afaa26
Mehmed Muric <mehmedmuric22@gmail.com>
2026-06-17 23:22:35 +0200

    first commit

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,22 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+// import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
 
-const RootLayout = () => {
-  return (
-    <View style={styles.container}>
-      <Text>_layout</Text>
-    </View>
-  )
-}
+// const RootLayout = () => {
+//   return (
+//     <View style={styles.container}>
+//       <Text>_layout</Text>
+//     </View>
+//   )
+// }
 
-export default RootLayout
+// export default RootLayout
 
-const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-})
+// const styles = StyleSheet.create({
+//     container: {
+//         display: 'flex',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         flex: 1,
+//         backgroundColor: '#fff',
+//     },
+// })
